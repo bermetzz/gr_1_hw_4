917af1f4aa0b3d45f2a168461452d1f959ea95c7
bermetzz <zulfibaevab@gmail.com>
2022-10-13 23:49:36 +0600

    extra tasks

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static int bossHealth = 700;
     public static int bossDamage = 50;
     public static String bossDefenceType;
-    public static int[] heroesHealth = {250, 270, 280, 300};
-    public static int[] heroesDamage = {25, 20, 15, 10};
-    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic","Medic"};
+    public static int[] heroesHealth = {250, 270, 280, 300, 900, 220, 230, 218};
+    public static int[] heroesDamage = {25, 20, 15, 10, 2, 11, 21, 18};
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic","Medic", "Golem", "Lucky", "Berserk", "Thor"};
     public static int roundNumber = 0;
     public static int healthCure=100;
+    public static int dodgeDamage;
 
 
     public static void main(String[] args) {
@@ -39,7 +40,7 @@ public class Main {
 
     public static void chooseBossDefence() {
         Random random = new Random();
-        int randomIndex = random.nextInt(heroesAttackType.length-1);
+        int randomIndex = random.nextInt(heroesAttackType.length);
         bossDefenceType = heroesAttackType[randomIndex];
     }
 
@@ -63,17 +64,43 @@ public class Main {
     }
 
     public static void bossHits() {
+        Random random=new Random();
+        boolean luckyChance=random.nextBoolean();
+        boolean oneRoundSkip=random.nextBoolean();
+        int golemsPart=0;
+        int bossHit=bossDamage;
         for (int i = 0; i < heroesHealth.length; i++) {
             if (heroesHealth[i] > 0) {
-                if (heroesHealth[i] - bossDamage < 0) {
+                if(!(heroesAttackType[i].equals("Golem"))){
+                    int partDamage=bossDamage/5;
+                    golemsPart+=partDamage;
+                    bossHit=bossDamage-bossDamage/5;
+                }
+
+                if(heroesAttackType[i].equals("Lucky") && luckyChance){
+                    bossHit=0;
+                    System.out.println("Lucky had a chance to dodge boss attacks!");
+                }
+                if(heroesAttackType[i].equals("Berserk")){
+                    int berserkPart = (int) (0.2*bossHit);
+                    dodgeDamage=bossHit- berserkPart;
+                    heroesHealth[i]-= berserkPart;
+                }
+                if(heroesAttackType[i].equals("Thor") && oneRoundSkip){
+                    System.out.println("one round is skipped");
+                    bossHit=0;
+                }
+                if (heroesHealth[i] - bossHit < 0) {
                     heroesHealth[i] = 0;
                 } else {
-                    heroesHealth[i] = heroesHealth[i] - bossDamage;
+                    heroesHealth[i] = heroesHealth[i] - bossHit;
+                }
+                if(heroesAttackType[i].equals("Golem")){
+                    heroesHealth[i]-=golemsPart;
                 }
             }
         }
     }
-
     public static void docCures(){
         for (int i=0; i<heroesHealth.length; i++){
             if (heroesHealth[i]<100 && heroesHealth[3]>0 && !(heroesAttackType[i].equals("Medic"))){
@@ -96,13 +123,16 @@ public class Main {
                     hit = heroesDamage[i] * coefficient;
                     System.out.println("Critical damage: " + hit);
                 }
+                if(heroesAttackType[i].equals("Berserk")){
+                    hit+=dodgeDamage;
+                }
                 if (bossHealth - hit < 0) {
                     bossHealth = 0;
                 } else {
                     bossHealth = bossHealth - hit;
                 }
-
             }
         }
     }
 }
+
